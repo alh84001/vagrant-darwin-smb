6074cffc81835729d337fb89c6fe9e6221b073a4
Matija Kovacic <matija.kovacic@fiveminutes.eu>
2015-05-25 15:48:56 +0200

    Mounting to paths where user has no write permissions (e.g. /)

--- lib/vagrant-darwin-smb/cap/mount_smb_shared_folder.rb
+++ lib/vagrant-darwin-smb/cap/mount_smb_shared_folder.rb
@@ -12,8 +12,16 @@ module VagrantDarwinSMB
       extend Vagrant::Util::Retryable
       def self.mount_smb_shared_folder(machine, name, guestpath, options)
         expanded_guest_path = machine.guest.capability(:shell_expand_guest_path, guestpath)
-        # create the guest path if it doesn't exist
-        machine.communicate.execute("mkdir -p #{expanded_guest_path}")
+        
+        mount_point_owner = options[:owner];
+        if mount_point_owner
+          machine.communicate.sudo("mkdir -p #{expanded_guest_path}")
+          machine.communicate.sudo("chown #{mount_point_owner} #{expanded_guest_path}")
+        else
+          # fallback to assumption that user has permission
+          # to create the specified mountpoint
+          machine.communicate.execute("mkdir -p #{expanded_guest_path}")
+        end
 
         smb_password = Shellwords.shellescape(options[:smb_password])
         mount_opts = options[:mount_options];
@@ -21,7 +29,7 @@ module VagrantDarwinSMB
           (mount_opts ? "-o '#{mount_opts.join(",")}' " : "") +
           "'//#{options[:smb_username]}:#{smb_password}@#{options[:smb_host]}/#{name}' " +
           "#{expanded_guest_path}"
-        retryable(on: Errors::DarwinMountFailed, tries: 10, sleep: 5) do 
+        retryable(on: Errors::DarwinMountFailed, tries: 10, sleep: 2) do
           machine.communicate.execute(
             mount_command,
             error_class: Errors::DarwinMountFailed)
